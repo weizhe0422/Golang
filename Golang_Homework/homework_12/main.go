a6c6c9cc0e5f765958864a39c72981635bb59961
weizhe0422 <weizhe.chang@gmail.com>
2017-06-07 00:29:19 +0800

    20170607 upload

--- Golang_Homework/homework_12/main.go
+++ Golang_Homework/homework_12/main.go
@@ -26,17 +26,17 @@ func main() {
 	fmt.Println("KEY: ", key)
 	cipher, err := crypto.NewAESWith([]byte(key), mode) //创建一个AES 加密的builder
 	if err != nil {
-		errors.New("ERROR: 创建一个AES 加密的builder")
+		errors.New("ERROR: 建立AES加密的builder")
 		return
 	}
 
-	planttext := `故经之以五事，校之以计而索其情：一曰道，二曰天，三曰地，四曰将，五曰法。道者，令民与上同意也，故可与之死，可与之生，而不畏危。天者，阴阳、寒暑、时制也。地者，高下、远近、险易、广狭、死生也。将者，智、信、仁、勇、严也。法者，曲制、官道、主用也。凡此五者，将莫不闻，知之者胜，不知者不胜。故校之以计而索其情，曰：主孰有道？将孰有能？天地孰得？法令孰行？兵众孰强？士卒孰练？赏罚孰明？吾以此知胜负矣。`
+	planttext := `我愛台灣`
 
 	ciphertext := cipher.Encrypt([]byte(planttext))
 
 	ciphertextWithBase64 := base64.URLEncoding.EncodeToString(ciphertext)
 
-	fmt.Println(ciphertextWithBase64)
+	fmt.Println("Ciphertext:", ciphertextWithBase64)
 
 	ciphertext, err = base64.URLEncoding.DecodeString(ciphertextWithBase64)
 	if err != nil {
